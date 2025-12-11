e05e4d2a183582aa5d37d2ba4ab9dbde82f4a3c3
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2025-12-10 22:16:14 -0500

    Update readme

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "emotion3459/Bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
